edf860bbc6f2dac16435337e80c62c1c775047a9
BeatrizMachado <beatriz.machadosilva@gmail.com>
2021-10-22 19:15:30 -0300

    up

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,33 +1,40 @@
 // FUNCAO DO APP - PEGAR CEP, ACHAR ENDEREÇO e CUSPIR HTML.
 
 // - PEGAR O CEP
-function pegarcep(){
+function PegarCep(){
     var cep = $("#cepinput").val()
     return cep
 }
 // - PESQUISAR
-async function pesquisarcep(){
-    // cep = await pegarcep();
+async function PesquisarCep(cep){
     resposta = await axios(`https://viacep.com.br/ws/${cep}/json/`)
     resultado = resposta.data
     return resultado
 }
 // - CUSPIR RESULTADO
-async function gerarhtml(){
-    pesquisa = await pesquisarcep()
+async function GerarHTML(address){
     $("#resultado").html(`
-        <h1 class="info">${pesquisa.cep}</h1>
-        <h1 class="info">${pesquisa.logradouro}</h1>
-        <h1 class="info">${pesquisa.complemento}</h1>
-        <h1 class="info">${pesquisa.bairro}</h1>
-        <h1 class="info">${pesquisa.localidade}</h1>
-        <h1 class="info">${pesquisa.uf}</h1>
-        <h1 class="info">${pesquisa.ddd}</h1>
+        <h1 class="info">${address.cep}</h1>
+        <h1 class="info">${address.logradouro}</h1>
+        <h1 class="info">${address.complemento}</h1>
+        <h1 class="info">${address.bairro}</h1>
+        <h1 class="info">${address.localidade}</h1>
+        <h1 class="info">${address.uf}</h1>
+        <h1 class="info">${address.ddd}</h1>
         `)
+    return
+}
+async function ImprimirResultados(){
+    cep = PegarCep()
+    address = await PesquisarCep(cep)
+    html = GerarHTML(address)
 }
 // - CHAMADAS
 $(document).ready(function(){
     console.log("RODANDO!");
 });
 
+
+//COISAS A FAZER:
+//- SUBSTITUIR CHAMADAS POR PARAMETROS
 
